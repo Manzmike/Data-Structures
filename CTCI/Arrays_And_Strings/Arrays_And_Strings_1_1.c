4cc38480f04bca3ec7345e95b8d60b67dd829954
Manzmike <mikelindsayengineering@gmail.com>
2024-10-26 14:55:22 -0700

    Updating the file sturcture for both CTCI and Leetcode

---
+++ CTCI/Arrays_And_Strings/Arrays_And_Strings_1_1.c
@@ -0,0 +1,9 @@
+// Section: Arrays + Strings
+// Problem 1.1
+// Question: Impliment an alg to determine if a strng has all unqie chars. What if you can't use addtional data structuress
+
+#include <stdio.h>
+
+int main(){
+    
+}
